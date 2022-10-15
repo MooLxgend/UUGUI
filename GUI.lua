a1a95975eaf36eee028c67b156922ec64d76e6ed
Ashton Schwenke <ashtonschwenke@gmail.com>
2022-10-15 15:46:14 -0400

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -634,17 +634,16 @@ local function WNAOR_fake_script()
 			for v23, v24 in pairs(u3) do
 				if v24.MinLevel <= v22 and v22 - 0.1 < v24.MinLevel then
 					l__Parent__1.ReachedTier:Play();
-					for v25, v26 in pairs(v13) do
-					v26.Position = UDim2.new(0, 0, v26.Position.Y.Scale + 0.05, 0);
-					end;
-				else
-					for v25, v26 in pairs(v13) do
-					v26.Position = UDim2.new(0, 0, v26.Position.Y.Scale + 0.2, 0);
-					end;
 				end;
+			end;
+			for v25, v26 in pairs(v13) do
+			v26.Position = UDim2.new(0, 0, v26.Position.Y.Scale + 0.1, 0);
 			end;
 			l__Rerolling__2.Ability.TextLabel.Text = decimalformat(math.floor(v22 * 10 + 0.5) / 10);
 			game:GetService("RunService").Heartbeat:wait();
+			end;
+					for v25, v26 in pairs(v13) do
+			v26.Position = UDim2.new(0, 0, v26.Position.Y.Scale + 0.05, 0);
 			end;
 			l__Rerolling__2.Ability.TextLabel.TextColor3 = v18;
 			l__Rerolling__2.Ability.ImageColor3 = v18;
